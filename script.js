2ba5149bb7759e9f1082220fe050207698774c8e
beardedbinary <carlball28@gmail.com>
2020-07-01 13:06:37 +0100

    removed additional CSS and comments from js file

--- script.js
+++ script.js
@@ -15,9 +15,3 @@ setGradient();
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
-
-//Write code so that the colour inputs match the background generated on the first page load.
-
-//Display the initial CSS linear gradient property on page load.
-
-//BONUS: Add a random button which generates two random numbers for the colour inputs.
